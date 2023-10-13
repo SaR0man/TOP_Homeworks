0664d7d8c8212e6fd32a7fe0fa7c3d0542b826da
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2023-10-13 21:33:12 +0300

    update

--- src/lesson_2023_10_08/Homework.java
+++ src/lesson_2023_10_08/Homework.java
@@ -1,7 +1,30 @@
 package lesson_2023_10_08;
 
+import java.util.ArrayList;
+import java.util.List;
+
+interface MyFunctional {
+    boolean test(int num);
+}
+
 public class Homework {
+    public static int sumAll(List<Integer> arr, MyFunctional predicate1, MyFunctional predicate2){
+        int sum = 0;
+        for (int num:arr) {
+            if (predicate1.test(num) && predicate2.test(num)){
+                sum+=num;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> arr = new ArrayList<>();
+        System.out.print("Контрольный вывод массива: ");
+        for (int i = 0; i < 10; i++) {
+            arr.add(i + 1);  // заполняем удобными для восприятия значениями от 1 до 10
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println("Сумма: " + sumAll(arr, n -> n > 2, n -> n < 5));
     }
 }
