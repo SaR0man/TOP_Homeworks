4ef57aec601a2fabe82b9c3b4daa6c7c52ce7dfe
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2023-10-26 20:45:05 +0300

    add lesson_2023_10_22

---
+++ src/lesson_2023_10_22/Library.java
@@ -0,0 +1,284 @@
+package lesson_2023_10_22;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Library {
+    ////// --= Поле класса =--
+    private Map<String, Map<String, List<String>>> library;  // библиотека словарей в формате словаря Map
+//    private List<Integer> numerator;  // коллекция для хранения перемешанных индексов для работы с wordOrigin и wordTrans
+//    private List<String> wordTrans;  // коллекция всех слов-переводов всей библиотеки
+//    private List<String> wordOrigin;  // коллекция оригинальных слов, соответствующих словам-переводам в wordTrans
+
+    ////// --= Блок инициализации =--
+    {
+        this.library = new TreeMap<>();
+//        this.numerator = new ArrayList<>();
+//        this.wordTrans = new ArrayList<>();
+//        this.wordOrigin = new ArrayList<>();
+
+    }
+
+    ////// --= Методы класса =--
+
+    //// Метод для добавления словаря
+    public boolean addDict(String dictionary) {  // возвращает true или false, принимает наименование словаря
+        dictionary = dictionary.toLowerCase();  // приводим переданное наименование к нижнему регистру
+        if (library.containsKey(dictionary)) {  // проверяем: нет ли уже такого словаря
+            return false;  // если есть, то ничего не делать и вернуть false
+        }
+        library.put(dictionary, new TreeMap<String, List<String>>());  // иначе поместить в библиотеку переданный словарь, где dictionary - это ключ, а new TreeMap<String, List<String>>() - значение.
+        return true;
+    }
+
+    //// Метод для удаления словаря
+    public boolean removeDict(String dictionary) {
+        dictionary = dictionary.toLowerCase();
+        if (library.containsKey(dictionary)) {  // если в библиотеке есть такой словарь, то удалить
+            library.remove(dictionary);
+            return true;
+        }
+        return false;
+    }
+
+    //// Метод добавляет новую словарную карточку в словарь
+    public boolean addCard(String dictionary, String original, String translation) {  // передается название словаря, оригинальное слово и слово-перевод
+        original = original.toLowerCase();
+        dictionary = dictionary.toLowerCase();
+        translation = translation.toLowerCase();
+        if (library.containsKey(dictionary)) {  // если библиотека содержит переданный словарь...
+            if (library.get(dictionary).containsKey(original)) {  // ... и если словарь содержит такой ключ...
+                if (library.get(dictionary).get(original).contains(translation)) {  // ... и содержит такой перевод
+                    return false;
+                } else {  // если такого translation переданного original нет, то заносим значение
+                    library.get(dictionary).get(original).add(translation);
+                    return true;
+                }
+            } else {  // если словарь не содержит такой ключ (словарную карточку)...
+                ArrayList<String> list = new ArrayList<>();  // ... создаем
+                list.add(translation);
+                library.get(dictionary).put(original, list);
+                return true;
+            }
+        } else {  // если библиотека НЕ содержит переданный словарь
+            if (addDict(dictionary)) {  // если метод addDict(dictionary) вернул true
+                return addCard(dictionary, original, translation);  // обращаемся к методу добавления словарной карточки и возвращаем результат операции
+            } else {  // если метод addDict(dictionary) вернул false
+                return false;
+            }
+        }
+    }
+
+    //// Метод-конвертер содержимого List<String> list в List<String> listArr с приведением всех записей в нижний регистр
+    private List<String> toLowerCase(List<String> list) {  // метод приватный, т.к. мы к нему не обращаемся из Main, к нему прямой доступ не нужен
+        List<String> listArr = new ArrayList<String>();  // Создает список с элементами типа String и сохраняет его в переменную listArr. Для этого используется класс ArrayList, который является подклассом класса List.
+        for (String word : list) {  // каждая запись из переданного в метод list
+            listArr.add(word.toLowerCase());  // добавляется в созданный выше listArr, при этом слова приводятся к нижнему регистру
+        }
+        return listArr;
+    }
+
+    //// Метод для сверки содержимого имеющейся коллекции слов-переводов и новой коллекции слов-переводов
+    private List<String> addNewWord(List<String> currentWordArr, List<String> newWordArr) {
+        for (String newWord : newWordArr) {
+            if (!currentWordArr.contains(newWord)) {  // если слова из новой коллекции нет в коллекции имеющихся слов-переводов...
+                currentWordArr.add(newWord);  // ... добавляем его в нее
+            }
+        }
+        return currentWordArr;
+    }
+
+    //// Перегруженный метод addCard добавляет словарную карточку, если значений перевода несколько
+    public boolean addCard(String dictionary, String original, List<String> translation) {  // передается название словаря, оригинальное слово и коллекция слов-переводов
+        dictionary = dictionary.toLowerCase();
+        original = original.toLowerCase();
+        translation = toLowerCase(translation);  // с помощью метода toLowerCase(List<String> list) приводим записи коллекции к нижнему регистру
+        if (library.containsKey(dictionary)) {  // если библиотека уже содержит такой словарь...
+            if (library.get(dictionary).containsKey(original)) {  // ... и если словарь уже содержит такое оригинальное слово
+                List<String> tempArr = library.get(dictionary).get(original);  // создаем коллекцию строк tempArr (имеющихся слов-переводов) для сравнения их с новыми предлагаемыми словами-переводами
+                List<String> newTempArr = addNewWord(tempArr, translation);  // вызов метода addNewWord по добавлению новых слов из translation в tempArr и присвоение результата его работы в коллекцию newTempArr
+                if (newTempArr.size() != tempArr.size()) {
+                    library.get(dictionary).put(original, newTempArr);  // поместить в переданный словарь переданное оригинальное слово и коллекцию новых слов-переводов
+                    return true;
+                }
+                return false;
+            } else {  // ... и если словарь еще не содержит такое оригинальное слово
+                library.get(dictionary).put(original, translation);  // поместить в переданный словарь переданные оригинальное слово и коллекцию слов-переводов
+                return true;
+            }
+        } else {  // если библиотека еще не содержит такой словарь
+            if (addDict(dictionary))  //  добавить такой словарь методом addDict, и если true...
+                return addCard(dictionary, original, translation);  // ... добавить словарную карточку методом addCard и вернуть успех или неуспех этой операции
+            else return false;
+        }
+    }
+
+    //// Метод выводит имеющиеся словари
+    public void printAllDicts() {
+        System.out.println("Имеются следующие словари:");
+        int count = 1;
+        for (String dictionary : library.keySet()) {  // простой перебор имеющихся ключей в библиотеке
+            System.out.println(count++ + ") " + dictionary);
+        }
+    }
+
+    //// Метод выводит содержимое всех имеющихся словарей
+    public void printLibrary() {
+        int count = 1;
+//        System.out.println();
+        for (String newKey : library.keySet()) {  // перебор всех словарей библиотеки
+//            System.out.println("-------------------------");
+            System.out.println(count++ + ") Содержимое словаря " + newKey + ":");
+            for (String original : library.get(newKey).keySet()) {  // перебор всех ключей (оригинальных слов) в словаре
+                System.out.print(original + " - ");
+                List<String> get = library.get(newKey).get(original);
+                for (int i = 0; i < get.size(); i++) {
+                    String translation = get.get(i);  // перебор всех слов-переводов в коллекциях original
+                    System.out.print(translation);
+                    if (i < get.size() - 1) System.out.print(", ");  // запятые ставим там, где они нужны
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    //// TODO ДЗ: метод выводит содержимое конкретного словаря
+    public void printCards(String dictionary) {
+        dictionary = dictionary.toLowerCase();
+        System.out.println("-------------------------");
+        System.out.println("Содержимое словаря " + dictionary + ":");
+        int count = 1;
+        for (String original : library.get(dictionary).keySet()) {  // перебор всех ключей (оригинальных слов) в словаре
+            System.out.print(count++ + ") " + original + " - ");
+            List<String> get = library.get(dictionary).get(original);
+            for (int i = 0; i < get.size(); i++) {
+                String translation = get.get(i);  // перебор всех слов-переводов в коллекциях original
+                System.out.print(translation);
+                if (i < get.size() - 1) System.out.print(", ");  // запятые ставим там, где они нужны
+            }
+            System.out.println();
+        }
+    }
+
+
+    //// TODO ДЗ: Метод удаляет словарную карточку
+    public boolean removeCard(String dictionary, String original) {
+        dictionary = dictionary.toLowerCase();
+        original = original.toLowerCase();
+        if (library.containsKey(dictionary)) {  // если переданный словарь имеется в библиотеке...
+            if (library.get(dictionary).containsKey(original)) {  // ... и если словарь содержит такое оригинальное слово...
+                library.get(dictionary).remove(original);  // ... то удаляем его
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //// TODO ДЗ: Метод удаляет слово-перевод
+    public boolean removeTrans(String dictionary, String original, String translation) {
+        original = original.toLowerCase();
+        dictionary = dictionary.toLowerCase();
+        translation = translation.toLowerCase();
+        if (library.containsKey(dictionary)) {  // если библиотека содержит переданный словарь...
+            if (library.get(dictionary).containsKey(original)) {  // ... и если словарь содержит такой ключ...
+                if (library.get(dictionary).get(original).contains(translation)) {  // ... и содержит такой перевод
+                    int size = library.get(dictionary).get(original).size();
+//                    System.out.println("Слов-переводов в слове \'" + translation + "\': " + size);
+                    if (size < 2) {  // если слово-перевод только одно...
+                        library.get(dictionary).remove(original);  // ... удаляем словарную карточку полностью
+                    } else {
+                        library.get(dictionary).get(original).remove(translation);  // иначе удаляем только слово-перевод
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //// TODO ДЗ: Метод выводит все словарные карточки указанного словаря, начинающиеся на переданный образец
+    public void printContains(String dictionary, String sample) {
+        dictionary = dictionary.toLowerCase();
+        sample = sample.toLowerCase();
+        System.out.println("Словарные карточки словаря \'" + dictionary + "\', начинающиеся с \'" + sample + "\':");
+        int count = 0;
+        for (String original : library.get(dictionary).keySet()) {  // перебор всех слов-ключей переданного словаря
+            if (original.startsWith(sample)) {  // если слово-ключ начинается с образца...
+                count++;
+                System.out.print(original + " - ");
+                List<String> get = library.get(dictionary).get(original);
+                for (int i = 0; i < get.size(); i++) {
+                    String translation = get.get(i);  // перебор всех слов-переводов в коллекциях original
+                    System.out.print(translation);
+                    if (i < get.size() - 1) System.out.print(", ");  // запятые ставим там, где они нужны
+                }
+                System.out.println();
+            }
+        }
+        if (count == 0) System.out.println("Слов, начинающихся с \'" + sample + "\' не найдено");
+    }
+
+    //// Метод, возвращающий наименование словаря по индексу его расположения в библиотеке
+    public String getDictByIndex(String strIndex) {
+        int index = 0;
+        index = Integer.parseInt(strIndex);
+        int count = 1;  // индекс словарей начинается с единицы
+        if (index <= library.keySet().size() && index > 0) {  // Проверяем, что введенный индекс не более количества словарей (т.е. размера библиотеки)
+            for (String dictionary : library.keySet()) {
+                if (index == count) return dictionary;
+                count++;
+            }
+        }
+        return null;
+    }
+
+
+
+//    //// Метод, создающий служебный словарь, состоящий из всех словарных карточек всех словарей
+//    private Map<String, List<String>> serverMap() {
+//        Map<String, List<String>> serverMap = new HashMap<>();
+//        return serverMap;
+//    }
+
+    //// Метод, возвращающий ключ из служебного словаря по переданному индексу
+
+//    //// TODO ДЗ: метод-тренажер
+//    public void train() {
+//        for (String newKey : library.keySet()) {  // перебор всех словарей библиотеки
+//            for (String original : library.get(newKey).keySet()) {  // перебор всех ключей (оригинальных слов) в словаре
+//                for (String translation : library.get(newKey).get(original)) {
+//                    wordTrans.add(translation);
+//                    wordOrigin.add(original);
+//                }
+//            }
+//        }
+////        System.out.println(wordTrans);
+////        System.out.println(wordOrigin);
+//        int size = wordTrans.size();
+//        for (int i = 0; i < size; i++) {
+//            numerator.add(i);
+//        }
+//        Collections.shuffle(numerator);
+////        System.out.println(numerator);
+//
+//
+//        System.out.println("Вводите перевод каждого слова, или нажмите 1 для выхода.");
+//        boolean switcher = true;
+//        while (switcher) {
+//            for (int i : numerator) {
+//                System.out.print(wordTrans.get(i) + " = ");
+//                Scanner scanner = new Scanner(System.in);
+//                String input = scanner.nextLine();
+//                if (input.equals("1")) {
+//                    System.out.println("Работа тренажера завершена.");
+//                    switcher = false;
+//                    break;
+//                } else if (input.equalsIgnoreCase(wordOrigin.get(i))) System.out.println("Верно!");
+//                else System.out.println("Неверно!");
+////                if (!switcher) break;
+//            }
+//        }
+//    }
+}
---
+++ src/lesson_2023_10_22/Main.java
@@ -0,0 +1,168 @@
+package lesson_2023_10_22;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("======= СЛОВАРЬ =========");
+        Scanner scanner = new Scanner(System.in);
+        String action;
+        Library library = new Library();  // создаем собственно объект библиотеки
+
+        do {
+            System.out.println(".........................");
+            System.out.println("1 Добавить словарь");
+            System.out.println("2 Добавить словарную карточку");
+            System.out.println("3 Добавить словарную карточку с переводом из несколько слов");
+            System.out.println("4 Напечатать список словарей в библиотеке");
+            System.out.println("5 Напечатать содержимое словаря");
+            System.out.println("6 Удалить словарь");
+            System.out.println("7 Поиск слова");
+            System.out.println("8 Выход");
+            System.out.print(">_");
+
+            action = scanner.nextLine();
+
+            if (action.equals("1")) {
+                System.out.print("Введите направление словаря, например, \'en-ru\' >_");
+                String dictionary = scanner.nextLine();
+                System.out.println(library.addDict(dictionary));
+                continue;
+            }
+
+            if (action.equals("2")) {
+                library.printAllDicts();
+                System.out.print("Введите номер словаря, в который добавляем словарную карточку >_");
+                String choiceDict = scanner.nextLine();
+                String lang = library.getDictByIndex(choiceDict);
+                System.out.print("Введите оригинальное слово >_");
+                String newOriginWord = scanner.nextLine();
+                System.out.print("Введите слово-перевод >_");
+                String newTransWord = scanner.nextLine();
+                System.out.println(library.addCard(lang, newOriginWord, newTransWord));
+                continue;
+            }
+
+            if (action.equals("3")) {
+                library.printAllDicts();
+                System.out.print("Введите номер словаря, в который добавляем словарную карточку >_");
+                String choiceDict = scanner.nextLine();
+                String lang = library.getDictByIndex(choiceDict);
+                System.out.print("Введите оригинальное слово >_");
+                String newOriginWord = scanner.nextLine();
+                List<String> translation = new ArrayList<>();
+                do {
+                    System.out.print("Введите новое слово-перевод, или цифру 0 для выхода >_");
+                    String transWord = scanner.nextLine();
+                    if (transWord.equals("0")) break;
+                    translation.add(transWord);
+                } while (true);
+                System.out.println(library.addCard(lang, newOriginWord, translation));
+                continue;
+            }
+
+            if (action.equals("4")) {
+                library.printAllDicts();
+                continue;
+            }
+
+            if (action.equals("5")) {
+                library.printAllDicts();
+                System.out.print("Введите номер словаря для вывода его содержимого >_");
+                String choiceDict = scanner.nextLine();
+                String lang = library.getDictByIndex(choiceDict);
+                library.printCards(lang);
+                continue;
+            }
+
+            if (action.equals("6")) {
+                library.printAllDicts();
+                System.out.print("Введите номер словаря который нужно удалить >_");
+                String choiceDict = scanner.nextLine();
+                String remove = library.getDictByIndex(choiceDict);
+                System.out.println(library.removeDict(remove));
+                continue;
+            }
+
+            if (action.equals("7")) {
+                library.printAllDicts();
+                System.out.print("Введите номер словаря, в котором искать слово >_");
+                String choiceDict = scanner.nextLine();
+                String findDict = library.getDictByIndex(choiceDict);
+                System.out.print("Введите первые буквы искомого слова >_");
+                String findSample = scanner.nextLine();
+                library.printContains(findDict, findSample);
+                continue;
+            }
+
+            if (action.equals("8")) {
+                System.out.println("Program completed");
+                break;
+            } else System.out.println("Некорректный ввод. Вводите команду в диапазоне 1-8: ");
+
+
+        } while (true);
+
+
+//        Library library = new Library();  // создаем собственно объект библиотеки
+//        System.out.println("Добавляем словарь \'en-ru\': " + library.addDict("en-ru"));  // добавляем англо-русский словарь
+//        System.out.println("Добавляем словарь \'En-Ru\': " + library.addDict("En-Ru"));  // для проверки добавляем словарь-дубликат
+//        System.out.println("Добавляем словарь \'ru-ru\': " + library.addDict("ru-ru"));  // добавляем бесполезный словарь :-)
+//        System.out.println("Удаляем словарь \'ru-ru\': " + library.removeDict("ru-ru"));  // удаляем бесполезный словарь
+//        System.out.println("=========================");
+//        System.out.println("В словарь \'en-ru\' добавляем словарную карточку \'word\' - \'слово\': " + library.addCard("en-ru", "word", "слово"));
+//        System.out.println("В словарь \'en-ru\' добавляем словарную карточку \'Word\' - \'слово\': " + library.addCard("en-ru", "Word", "слово"));
+//        // НАПОЛНЯЕМ СЛОВАРИ ДАЛЕЕ без вывода в консоль
+//        library.addCard("en-ru", "time", "время");
+//        library.addCard("en-ru", "home", "дом");
+//        library.addCard("en-ru", "cat", "кот");
+//        library.addCard("en-ru", "cat", "кошка");
+//        library.addCard("en-ru", "land", "земля");
+//        library.addCard("en-ru", "hand", "рука");
+//        library.addDict("ru-en");  // создаем русско-английский словарь
+//        ArrayList<String> tempTrans = new ArrayList<>();  // создаем коллекцию для нескольких значений перевода
+//        tempTrans.add("house");
+//        tempTrans.add("home");
+//        library.addCard("ru-en", "дом", tempTrans);  // используем метод, принимающий сразу коллекцию переводов слова
+//        library.addCard("ru-en", "кот", "cat");
+//        System.out.println("=========================");
+//        System.out.println("Выводим все словари:");
+//        library.printAllDicts();
+//        System.out.println("=========================");
+//        System.out.println("Выводим содержимое всех словарей:");
+//        library.printLibrary();
+//        System.out.println("=========================");
+//        System.out.println("Работа с удалением словарных карточек и удалением слов-переводов:");
+//        library.addCard("ru-en", "голова", "*** ошибочная запись ***");  // для теста вводим словарную карточку с некорректным переводом
+//        library.printCards("ru-en");
+//        System.out.println("удаляем некорректную словарную карточку");
+//        System.out.println(library.removeCard("ru-en", "голова"));
+//        library.printCards("ru-en");
+//        library.addCard("ru-en", "голова", "*** ошибочная запись ***");  // для теста вводим словарную карточку с некорректным переводом
+//        library.addCard("ru-en", "дом","*** ошибочная запись ***");  // добавляем некорректное слово-перевод в коллекцию слов-переводов
+//        library.printCards("ru-en");
+//        System.out.println(library.removeTrans("ru-en", "голова", "*** ошибочная запись ***"));  // удаляем некорректное слово-перевод из словарной карточки с одним переводом
+//        library.printCards("ru-en");
+//        System.out.println(library.removeTrans("ru-en", "дом","*** ошибочная запись ***"));  // удаляем некорректное слово-перевод из словарной карточки с несколькими переводами
+//        library.printCards("ru-en");
+//        System.out.println("=========================");
+//        library.printContains("en-ru", "h");
+////        System.out.println("=========================");
+////        library.train();
+//        System.out.println("=========================");
+//        System.out.println("Тестируем добавление в словарную карточку сначала одного слова, затем коллекции слов:");
+//        library.addCard("en-ru", "child", "ребенок");
+//        tempTrans.clear();
+//        tempTrans.add("дитя");
+//        tempTrans.add("малыш");
+//        library.addCard("en-ru", "child", tempTrans);
+//        library.printCards("en-ru");
+//        library.printAllDicts();
+//        Scanner scanner = new Scanner(System.in);
+//        int index = scanner.nextInt();
+//        System.out.println(library.getDictByIndex(index));
+
+    }
+}
