229303d5bc8c2faf1e8b2e4e06996acc349d4173
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2023-11-12 22:09:50 +0300

    add lesson_2023_11_11

---
+++ src/lesson_2023_11_11_phoneBook/Main.java
@@ -0,0 +1,9 @@
+package lesson_2023_11_11_phoneBook;
+
+import lesson_2023_11_11_phoneBook.models.Person;
+
+public class Main {
+    Person person = new Person();
+
+
+}
---
+++ src/lesson_2023_11_11_phoneBook/models/Person.java
@@ -0,0 +1,12 @@
+package lesson_2023_11_11_phoneBook.models;
+
+public class Person {
+    public Person() {
+    }
+
+    private static int id;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+
+}
