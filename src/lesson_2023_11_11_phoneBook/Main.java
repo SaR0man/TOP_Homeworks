e6e72b6b3634126a2b74e43ae980c32d90993063
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2023-11-29 22:08:57 +0300

    update

--- src/lesson_2023_11_11_phoneBook/Main.java
+++ src/lesson_2023_11_11_phoneBook/Main.java
@@ -4,6 +4,22 @@ import lesson_2023_11_11_phoneBook.models.Contact;
 import lesson_2023_11_11_phoneBook.models.PhoneBook;
 import lesson_2023_11_11_phoneBook.models.Menu;
 
+/*
+PhoneBook
+Модель (id,name,surname,fathername,phone,bith,age , gender,Type(Mob,Home,Fax))
+Добавление
+Удаление
+Редактирование
+Поиск
+ * - 0 и больше символов
+ _ - 1 символ
+- F*   -> Farid , Fedya , F
+- F*Z  -> FaaasoakdofkowZ FZ
+- F _ _ id -> Farid , Fozid
+Фильтрация
+Сортировка
+Сохранение было! (в фаил формат любой , txt,xml,json)
+ */
 
 //import models.Contact;
 //import models.Menu;
